79d92188c8daad9be83bb86437f473910aec37a0
Aria <ariagudlaug@gmail.com>
2018-08-11 17:49:45 +0100

    Added game manager class and player class (might not be needed), also added preloadbehaviour

---
+++ StrategyGame/Assets/Scripts/Managers/Game/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GametManager instance;
+
+	[HideInInspector] private Player player1;
+	[HideInInspector] private Player player2;
+
+	[HideInInspector] private int actionsPerTurn = 3;
+	[HideInInspector] private int currentTurn;
+	[HideInInspector] private int currentActivePlayer;
+
+
+	// Use this for initialization
+	void Start () {
+
+		// Check if the instance of the input manager has already been set and respond according to singleton format
+		if(instance != null)
+		{
+			Destroy(this);
+		}
+		else
+		{
+			instance = this;
+		}
+
+		this.currentTurn = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
